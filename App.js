249ca763c2d10dc65c4f153276c7ce8be3bd2145
Scott <scrosehart@gmail.com>
2018-12-10 15:50:31 -0800

    working on testing different drawing libraries

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+//
+import Sketch from 'react-native-sketch';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.outer}>
+        <View style={styles.inner}>
+          <Text>React Native SVG</Text>
+        </View>
+        <View style={styles.inner}>
+          <Text>Expo Pixi</Text>
+        </View>
+        <View style={styles.inner}>
+          <Text>React Canvas Draw</Text>
+        </View>
+        <View style={styles.inner}>
+          <Text>React Native Sketch</Text>
+          {/* <Sketch
+            ref={sketch => this.sketch = sketch}
+            strokeColor='#ff69b4'
+            strokeThickness={3}
+          /> */}
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  outer: {
+    padding: 10,
+    paddingTop: 50,
+    paddingBottom: 50,
+    backgroundColor: 'yellow',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  inner: {
+    height: 300,
+    width: '100%',
+    marginBottom: 10,
+    backgroundColor: 'blue',
+  }
 });
