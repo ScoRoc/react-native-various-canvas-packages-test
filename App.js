7d8635d2264382b0e17a97a14a7a641cef752eb8
Scott <scrosehart@gmail.com>
2018-12-10 16:02:18 -0800

    testing more libs

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
-//
+import Svg, { Circle, Rect } from 'react-native-svg';
+import Expo from 'expo';
+import * as ExpoPixi from 'expo-pixi';
+import CanvasDraw from 'react-canvas-draw';
 import Sketch from 'react-native-sketch';
 
 export default class App extends React.Component {
@@ -9,12 +12,33 @@ export default class App extends React.Component {
       <ScrollView contentContainerStyle={styles.outer}>
         <View style={styles.inner}>
           <Text>React Native SVG</Text>
+          <Svg height={50} width={50} viewBox='0 0 100 100'>
+            {/* <Circle
+              cx='50'
+              cy='50'
+              r='45'
+              stroke='orange'
+              strokeWidth='2'
+              fill='green'
+            /> */}
+            <Rect
+              x='15'
+              y='15'
+              width='70'
+              height='70'
+              stroke='white'
+              strokeWidth='2'
+              fill='purple'
+            />
+          </Svg>
         </View>
         <View style={styles.inner}>
           <Text>Expo Pixi</Text>
+          <ExpoPixi.Sketch />
         </View>
         <View style={styles.inner}>
           <Text>React Canvas Draw</Text>
+          {/* <CanvasDraw /> */}
         </View>
         <View style={styles.inner}>
           <Text>React Native Sketch</Text>
